1e00dc9da1aa2d546e8d3e19c707fb9d0c78af9f
Denis789-4 <b0tt0miezz@gmail.com>
2021-11-03 16:13:20 +0200

    valid form

--- src/components/CartBillingForm.vue
+++ src/components/CartBillingForm.vue
@@ -1,32 +1,38 @@
 <template>
-  <form @submit.prevent="PlaceOrder();reloadPage()" class="row g-3 mb-3">
+  <form @submit.prevent="PlaceOrder()/*;reloadPage()*/" class="row g-3 mb-3">
     <h5>Billing details</h5>
     <div class="col-md-6">
       <label for="firstName" class="form-label">First Name</label>
       <input v-model="firstName" type="text" class="form-control" id="firstName" placeholder="First Name">
+      <span v-if="!isFirstNameValid">First name is not valid</span>
     </div>
     <div class="col-md-6">
       <label for="lastName" class="form-label">Last Name</label>
       <input v-model="lastName" type="text" class="form-control" id="lastName" placeholder="Last Name">
+      <span v-if="!isLastNameValid">Last name is not valid</span>
     </div>
     <div class="col-12">
       <label for="city" class="form-label">City</label>
       <input v-model="city" type="text" class="form-control" id="city" placeholder="City">
+      <span v-if="!isCityValid">City is not valid</span>
     </div>
     <div class="col-12">
       <label for="address" class="form-label">Address</label>
       <input v-model="address" type="text" class="form-control" id="address" placeholder="Address">
+      <span v-if="!isAddressValid">Address is not valid</span>
     </div>
     <div class="col-md-6">
       <label for="email" class="form-label">Email Address</label>
       <input v-model="email" type="email" class="form-control" id="email" placeholder="Email Address">
+      <span v-if="!isEmailValid">Email is not valid</span>
     </div>
     <div class="col-md-6">
       <label for="phone" class="form-label">Phone</label>
       <input v-model="phone" type="text" class="form-control" id="phone" placeholder="+38(099) 999-99-99">
+      <span v-if="!isPhoneValid">The Phone field must contain only numbers and dashes.</span>
     </div>
     <div class="col-12 d-flex justify-content-end">
-      <button type="submit" class="btn btn-success">
+      <button :disabled="!isFirstNameValid" type="submit" class="btn btn-success">
         Place Order
       </button>
     </div>
@@ -43,11 +49,17 @@ export default {
     address: "",
     email: "",
     phone: "",
+    isFirstNameValid: true,
+    isLastNameValid: true,
+    isCityValid: true,
+    isAddressValid: true,
+    isEmailValid: true,
+    isPhoneValid: true,
   }),
 
 
   methods: {
-    PlaceOrder(){
+    PlaceOrder() {
       console.log({
         firstName: this.firstName,
         lastName: this.lastName,
@@ -61,7 +73,40 @@ export default {
     reloadPage() {
       window.location.reload()
     },
-
+  },
+  watch: {
+    firstName(FValue) {
+      this.isFirstNameValid = true;
+      if (FValue.length > 10) this.isFirstNameValid = false;
+    },
+    lastName(LValue) {
+      this.isLastNameValid = true;
+      if (LValue.length > 10) this.isLastNameValid = false;
+    },
+    city(CValue) {
+      let regex = /^[А-ЯЁ][а-яё]+$/;
+      if(!regex.test(CValue)){
+        this.isCityValid = false;
+      }else{
+        this.isCityValid = true;
+      }
+    },
+    address(AValue) {
+      this.isAddressValid = true;
+      if (AValue.length > 10) this.isAddressValid = false;
+    },
+    email(EValue) {
+      let regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if(!regex.test(EValue)){
+        this.isEmailValid = false;
+      }else{
+        this.isEmailValid = true;
+      }
+    },
+    phone(PValue) {
+      this.isPhoneValid = true;
+      if (PValue.length === 13) this.isPhoneValid = false;
+    },
   }
 }
 </script>
